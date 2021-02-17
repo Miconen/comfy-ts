73080655b2793d3931a01c974635ba9c81b59013
Miconen <mico.rintala@gmail.com>
2021-02-17 18:02:57 +0200

    mvc

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 import * as Discord from "@typeit/discord";
+
+class ComfyModel {
+	constructor() {
+
+	}
+}
+
+class ComfyView {
+	constructor() {
+
+	}
+}
+
+class ComfyController {
+	constructor(model, view) {
+		this.model = model
+		this.view = view
+	}
+}
+
+const app = new Controller(new Model(), new View())
